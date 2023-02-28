fbe011cb0bf38126273a08385544623dee4c5a84
Sebastian Bejarano <s.bejarano.zarate@gmail.com>
2023-02-28 15:05:09 -0500

    subida del proyecto angular

---
+++ src/app/y/y.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-y',
+  templateUrl: './y.component.html',
+  styleUrls: ['./y.component.css']
+})
+export class YComponent {
+
+}
